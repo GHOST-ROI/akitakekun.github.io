0d747c62eb21cea10ce67a70d16d0df897f97ddd
Skraay <theskraay@gmail.com>
2014-06-12 20:42:06 +0200

    Website Files

---
+++ README.md
@@ -0,0 +1,8 @@
+Skraay's Website
+================
+
+A website I will use for the following:
+
+- Posting some of my upcoming ComputerCraft projects.
+- Teaching myself how to maintain and code a proper website.
+- More..
---
+++ javascripts/main.js
@@ -0,0 +1,4 @@
+console.log("Hello random guy who looks at the JavaScript console.");
+console.log("I guess you have your eyes open right now.");
+console.log("Just having fun but remember...");
+console.warn("I am watching you >:C");
