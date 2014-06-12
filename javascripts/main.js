a053837bba37be7e7a8504eeaff0c18ea08e123c
Akitake Desu <contact.akitakekun@gmail.com>
2014-06-12 20:43:51 +0200

    Replace master branch with page content via GitHub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,1 @@
-console.log("Hello random guy who looks at the JavaScript console.");
-console.log("I guess you have your eyes open right now.");
-console.log("Just having fun but remember...");
-console.warn("I am watching you >:C");
+console.log('This would be the main JS file.');
